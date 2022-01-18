39c9c7df646ccb2c5ea8ac158443577f3e0b4685
krishtoautomate <tulasikrishna89@gmail.com>
2022-01-18 17:38:45 -0500

    update DesiredCapability matcher to udid

--- src/main/java/com/wdio/qa/grid/matcher/CapabilityMatcher.java
+++ src/main/java/com/wdio/qa/grid/matcher/CapabilityMatcher.java
@@ -1,48 +1,33 @@
 package com.wdio.qa.grid.matcher;
 
-import org.openqa.grid.internal.utils.DefaultCapabilityMatcher;
-
 import java.util.Map;
-import java.util.Objects;
+import org.openqa.grid.internal.utils.DefaultCapabilityMatcher;
 
 public class CapabilityMatcher extends DefaultCapabilityMatcher {
 
-    @Override
-    public boolean matches(Map<String, Object> nodeCapability, Map<String, Object> requestedCapability) {
-        final String deviceName = "deviceName";
-        final String deviceNameAppium = "appium:" + deviceName;
-        final String browserName = "browserName";
+  @Override
+  public boolean matches(Map<String, Object> nodeCapability,
+      Map<String, Object> requestedCapability) {
 
-        // If the request does not have the special capability,
-        // we return what the DefaultCapabilityMatcher returns
-        boolean result;
-        if (requestedCapability.containsKey(deviceName)) {
-            result = matcher(nodeCapability, requestedCapability, deviceName, deviceName);
-        } else if (requestedCapability.containsKey(deviceNameAppium)) {
-            result = matcher(nodeCapability, requestedCapability, deviceName, deviceNameAppium);
-        } else if (requestedCapability.containsKey(browserName)) {
-            result = matcher(nodeCapability, requestedCapability, browserName, browserName);
-        } else {
-            System.out.println("Basic matcher");
-            result = super.matches(nodeCapability, requestedCapability);
-        }
+    String nodeName = "udid";
 
-        if (result) {
-            System.out.println("Node capabilities:\n" + nodeCapability + "\n===");
-        }
+    boolean basicChecks = super.matches(nodeCapability, requestedCapability);
 
-        return result;
+    // If the request does not have the special capability, we return what the
+    // DefaultCapabilityMatcher returns
+    if (!requestedCapability.containsKey(nodeName)) {
+      return basicChecks;
     }
 
-    private boolean matcher(Map<String, Object> nodeCapability, Map<String, Object> requestedCapability, String nCap, String rCap) {
-        if (!nodeCapability.containsKey(nCap)) return false;
-
-        String expected = (String) requestedCapability.get(rCap);
-        String actual = (String) nodeCapability.get(nCap);
-
-        boolean result = Objects.equals(expected, actual);
-        System.out.println(String.format("Is %s matching (A==E): '%s'=='%s'. Result: %s", nCap, actual, expected, result));
-
-        return result;
+    // We check that the node contains the special capability and if so, we try to match the
+    // capabilities
+    if (nodeCapability.containsKey(nodeName)) {
+      return basicChecks && nodeCapability.get(nodeName).equals(requestedCapability.get(nodeName));
     }
+
+    // If none of the previous matched, the node does not have the capabilities
+    return false;
+
+  }
+
 }
